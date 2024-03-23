2001b04e88b52b5ed1167459b772d4b5bcd95921
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-03-23 21:19:04 +0100

    ПЗ

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('Наше проміжне ПЗ');
+    next();
+});
+
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
