9eeae20ffb25b929cfcb30fb50bc8b6459f588e4
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-03-23 20:36:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,10 +5,21 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.get('/contact', (req, res) => {
+// app.get('/contact', (req, res) => {
+//     res.send('<h1>Contact page</h1>');
+// });
+
+// app.get('/con?tact', (req, res) => {
+//     res.send('<h1>Contact page</h1>');
+// });
+
+// app.get('/con+tact', (req, res) => {
+//     res.send('<h1>Contact page</h1>');
+// });
+
+app.get('/con*tact', (req, res) => {
     res.send('<h1>Contact page</h1>');
 });
-
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
